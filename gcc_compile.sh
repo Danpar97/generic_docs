adbf61f5ef5145a4cc4378d6859974ab45146411
danpar97 <danish.parvez19@gmail.com>
2018-03-19 20:01:59 +0530

    Minor changes
    Updated the compile script to include the commit hash
    
    syntactical corrections in contributing.md

--- gcc_compile.sh
+++ gcc_compile.sh
@@ -14,15 +14,15 @@ W_FLAGS='-Wall -Wextra -Werror -Wfatal-errors -Wpedantic -pedantic-errors -fdiag
 INC_FLAGS= # '-I/usr/include/SDL2'
 SDL_FLAGS= # '-lSDL2 -lSDL2main -lSDL2_image'
 
-# clear out old object files
-rm ./source/*.o
+# get hash of the last commit
+HASH=$(git log | head -n1 | cut -d " " -f 2 | cut -c 1-6)
+
+# clear out old binaries files
+rm ./binaries/Linux_Binary_*
 
 # compile
 $CPP $INC_FLAGS -c ./source/experimental.cpp -o ./source/experimental.o $W_FLAGS
-$CPP -o ./binaries/experimental_Linux ./source/experimental.o $SDL_FLAGS $W_FLAGS
+$CPP -o ./binaries/Linux_Binary_$HASH ./source/experimental.o $SDL_FLAGS $W_FLAGS
 
-# (outdated method) compile from terminal: g++ -o experimental ./experimental.cpp $(pkg-config --cflags --libs SDL2 SDL2main SDL2_image)
-
-# Two step compilation
-# g++  -I/usr/include/SDL2 -c ./source/experimental.cpp -o ./source/experimental.o
-# g++  -o ./source/experimental ./source/experimental.o  -lSDL2 -lSDL2main -lSDL2_image
+# clear out old object files
+rm ./source/*.o
